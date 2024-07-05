c8cbf97d20853bfd1755d2db373d01bc2d54f2b5
dalarcongfm <164048564+dalarcongfm@users.noreply.github.com>
2024-07-05 16:19:23 -0300

    Update originlogger.js

--- originlogger.js
+++ originlogger.js
@@ -13,7 +13,8 @@ const channel = new MessageChannel();
 window.addEventListener("message", function (event) {
   // We are receiveing messages from any origin, you can check of the origin by
   // using event.origin
-
+  console.log("origin message start --->");
+  console.log("ports: " + event.ports);
   // get the port then use it for communication.
   var port = event.ports[0];
   if (typeof port === 'undefined') return;
