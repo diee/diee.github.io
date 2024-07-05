4fcb1e8d2b94930b30dae3d6ab8ac96cf540736b
dalarcongfm <164048564+dalarcongfm@users.noreply.github.com>
2024-07-05 14:59:12 -0300

    Update application.js

--- application.js
+++ application.js
@@ -20,3 +20,16 @@ function testFunction() {
   document.getElementById("surprisePic").src = "images/icons/gon.png";
 };
 
+function testFunctionMessage() {
+  var port = event.ports[0];
+  if (typeof port === 'undefined') return;
+
+  // Post message on this port.
+  port.postMessage("Test")
+
+  // Receive upcoming messages on this port.
+  port.onmessage = function(event) {
+    console.log("[PostMessage1] Got message" + event.data);
+  };
+};
+
